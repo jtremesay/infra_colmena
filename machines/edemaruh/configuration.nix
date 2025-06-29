3069ad160dc485179e6c0f81b622b2f6509d6383
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-06-29 22:25:40 +0200

    add ollama to edemaruh

--- machines/edemaruh/configuration.nix
+++ machines/edemaruh/configuration.nix
@@ -19,6 +19,39 @@
   networking.hostName = "edemaruh";
   networking.networkmanager.enable = true;
 
+  # Ollama CUDA
+  services.ollama = {
+    enable = true;
+    acceleration = "cuda";
+    loadModels = [
+      "cogito:3b"
+      "cogito:8b"
+      "cogito:14b"
+      "cogito:32b"
+      "gemma3:1b"
+      "gemma3:4b"
+      "gemma3:12b"
+      "gemma3:27b"
+      "gemma3n:e2b"
+      "gemma3n:4b"
+      "granite3.3:2b"
+      "granite3.3:8b"
+      "llama4:16x17b"
+      "mistral-small3.2:24b"
+      "phi4:14b"
+      "phi4-reasoning:14b"
+      "phi4-mini:3.8b"
+      "phi4-mini-reasoning:3.8b"
+      "qwen3:0.6b"
+      "qwen3:1.7b"
+      "qwen3:4b"
+      "qwen3:8b"
+      "qwen3:14b"
+      "qwen3:30b"
+      "qwen3:32b"
+    ];
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
