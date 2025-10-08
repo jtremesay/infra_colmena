af60d3ef6096c44d121089695694329186baabcb
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-10-08 10:54:37 +0200

    update deps

--- machines/edemaruh/modules/ollama_server.nix
+++ machines/edemaruh/modules/ollama_server.nix
@@ -10,12 +10,15 @@
       "gemma3:4b"
       "gemma3n:e4b"
       "granite3.3:8b"
+      "granite4:tiny-h"
       "mistral:7b"
       "phi4-mini:3.8b"
       "phi4-mini-reasoning:3.8b"
       "qwen3:8b"
       # Embeddings
+      "embeddinggemma:300m"
       "nomic-embed-text:latest"
+      "qwen3-embedding:8b"
     ];
     host = "0.0.0.0";
   };
