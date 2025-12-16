7230408f1e993585061903fadb46da2242018624
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-12-16 23:25:11 +0100

    add mirror

--- machines/hiraeth/modules/mirrors/default.nix
+++ machines/hiraeth/modules/mirrors/default.nix
@@ -9,11 +9,29 @@
     "d /srv/http/mirrors 0755 root root -"
   ];
 
-  services.caddy.virtualHosts."mirrors.jtremesay.org" = {
+  services.caddy.virtualHosts."http://mirrors.jtremesay.org:8000" = {
     extraConfig = ''
       root * /srv/http/mirrors
       file_server browse
     '';
   };
 
+  services.traefik.dynamicConfigOptions = {
+    http = {
+      routers."mirrors" = {
+        rule = "Host(`mirrors.jtremesay.org`)";
+        service = "mirrors";
+        entryPoints = [ "https" ];
+        tls.certResolver = "le";
+      };
+
+      services."mirrors" = {
+        loadBalancer = {
+          servers = [
+            { url = "http://127.0.0.1:8000"; }
+          ];
+        };
+      };
+    };
+  };
 }
