ff3a920dbb0ca73aa25563783484b0239418b089
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2026-04-09 18:50:44 +0200

    fix mirrors

--- modules/mirrors/default.nix
+++ modules/mirrors/default.nix
@@ -21,13 +21,11 @@ in
       "d /srv/http/mirrors 0755 root root -"
     ];
 
-    services.caddy.virtualHosts."http://${cfg.host}:8000" = {
+    services.caddy.virtualHosts."${cfg.host}" = {
       extraConfig = ''
         root * /srv/http/mirrors
         file_server browse
       '';
     };
-
-    slaanesh.caddy.reverseProxies."${cfg.host}" = "127.0.0.1:8000";
   };
 }
